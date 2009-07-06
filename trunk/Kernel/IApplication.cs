629953af3adcced0f9f42507e367ce3056cae800
artem.shynkarenko <artem.shynkarenko@748823f4-67e9-11de-873c-fb8c264d332a>
2009-07-06 12:38:22 +0000

    Commit initial ideas

---
+++ trunk/Kernel/IApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JazCms.Kernel
+{
+	/// <summary>
+	/// This is root
+	/// </summary>
+	public interface IApplication
+	{
+	}
+}
---
+++ trunk/Kernel/IStructureElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace JazCms.Kernel
+{
+	[Serializable]
+	public interface IStructureElement:IJazCmsObject
+	{
+		Collection<IStructureElement> ChildElements { get; }
+		IStructureElement ParentElement { get; }
+		Collection<IStructureExtension> Extensions { get; }
+	}
+}
