064b2bd578a21cdaab24a154362078a3970d42eb
artem.shynkarenko <artem.shynkarenko@748823f4-67e9-11de-873c-fb8c264d332a>
2009-08-20 08:39:06 +0000

    Added stub project with XmlStoreProvider

---
+++ StoreProviders/XmlStore/XmlStoreProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JazCms.Kernel;
+
+namespace JazCms.StoreProviders.XmlStore
+{
+	public class XmlStoreProvider:ISettingStoreProvider, IStructureStoreProvider
+	{
+		#region ISettingStoreProvider Members
+
+		public void LoadSettings(ISettingOwner owner)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SaveSettings(ISettingOwner owner)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region IJazCmsObject Members
+
+		public Guid Identity
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string SystemName
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		#endregion
+	}
+}
