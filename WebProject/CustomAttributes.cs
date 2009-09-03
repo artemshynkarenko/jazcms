f9a69822ddf55847000697c1afbe9fbdabba18ea
ku3necvakula <ku3necvakula@748823f4-67e9-11de-873c-fb8c264d332a>
2009-09-03 06:45:53 +0000

    Added custom attributes.

---
+++ WebProject/CustomAttributes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JazCms.WebProject
+{
+    public class HidePropertyAttribute : System.Attribute
+    {
+
+        public bool IsHidden               
+        {
+            get
+            {
+                return isHidden;
+            }
+            set
+            {
+
+                isHidden = value;
+            }
+        }
+
+        public HidePropertyAttribute(bool hideProperty) 
+        {
+            isHidden = hideProperty;
+        }
+
+        private bool isHidden;
+    }
+}
