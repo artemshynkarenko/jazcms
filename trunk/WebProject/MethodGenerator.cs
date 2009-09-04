3ac18e8e91ea01d2721d8d0c1d7e0671ba2dbd00
ku3necvakula <ku3necvakula@748823f4-67e9-11de-873c-fb8c264d332a>
2009-09-04 14:28:31 +0000

    Adding new methods and properties is moved to certain classes.

---
+++ trunk/WebProject/MethodGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.CodeDom;
+using System.Reflection;
+
+namespace JazCms.WebProject
+{
+    public static class MethodGenerator
+    {
+        public static CodeMemberMethod GenerateMethod(MemberInfo member, CodeMethodInvokeExpression statment)
+        {
+            CodeMemberMethod methodCTM = new CodeMemberMethod();
+            methodCTM.Name = member.Name;
+            methodCTM.Attributes = MemberAttributes.Public | MemberAttributes.Final;
+            CodeTypeReference methodTypeRef = new CodeTypeReference(((MethodInfo)(member)).ReturnType);
+            methodCTM.ReturnType = methodTypeRef;
+
+            List<ParameterInfo> methodParameters = ((System.Reflection.MethodInfo)(member)).GetParameters().ToList();
+            CodeParameterDeclarationExpressionCollection parameterCollection = new CodeParameterDeclarationExpressionCollection();
+            foreach (ParameterInfo info in methodParameters)
+            {
+                CodeParameterDeclarationExpression expresion = new CodeParameterDeclarationExpression(info.ParameterType, info.Name);
+                parameterCollection.Add(expresion);
+            }
+            methodCTM.Parameters.AddRange(parameterCollection);
+            methodCTM.Statements.Add(statment);
+            return methodCTM;
+        }
+    }
+}
---
+++ trunk/WebProject/PropertyGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.CodeDom;
+using System.Reflection;
+
+namespace JazCms.WebProject
+{
+    public static class PropertyGenerator
+    {
+        public static CodeMemberProperty GenerateProperty(MemberInfo member,CodeMethodInvokeExpression getStatment, 
+                                                           CodeMethodInvokeExpression setStatment)
+        {
+            PropertyInfo property = member as PropertyInfo;
+            CodeMemberProperty propertyCTM = new CodeMemberProperty();
+            propertyCTM.Attributes = MemberAttributes.Public | MemberAttributes.Final;
+            propertyCTM.Name = member.Name;
+            CodeTypeReference typeRef = new CodeTypeReference(((PropertyInfo)(member)).PropertyType);
+            propertyCTM.Type = typeRef;
+            if (property.CanWrite)
+            {
+                propertyCTM.SetStatements.Add(setStatment);
+            }
+            if (property.CanRead)
+            {
+                propertyCTM.GetStatements.Add(getStatment);
+            }
+            return propertyCTM;
+        }
+
+    }
+}
