6d655eb1706d71c1ea15ef3af837864f7a5abe11
ku3necvakula <ku3necvakula@748823f4-67e9-11de-873c-fb8c264d332a>
2009-09-04 12:11:16 +0000

    Added code implementation of interface to new generated file.

--- trunk/WebProject/FileGenerator.cs
+++ trunk/WebProject/FileGenerator.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.CodeDom;
 using System.CodeDom.Compiler;
 using System.IO;
+using JazCms.Kernel;
+using System.Reflection;
 
 namespace JazCms.WebProject
 {
@@ -23,26 +25,69 @@ namespace JazCms.WebProject
             CodeTypeDeclaration jazClass = new CodeTypeDeclaration(JazClassName);
             jazClass.IsPartial = true;
             jazClass.IsClass = true;
-            jazClass.BaseTypes.Add("IPageInstance");
 
+            jazClass.BaseTypes.Add("IPageInstance");
+            List<Type> typeCollection = ((typeof(IPageInstance))).UnderlyingSystemType.GetInterfaces().ToList();
+
+            CodeTypeReferenceExpression csSystemConsoleType = new CodeTypeReferenceExpression("throw new System");
+            CodeMethodInvokeExpression cs1 = new CodeMethodInvokeExpression(
+            csSystemConsoleType, "NotImplementedException", new CodePrimitiveExpression());
+         
+            foreach (Type interf in typeCollection)
+            {
+                List<string> propertyNames = new List<string>();
+                IList<MemberInfo> memberInfoCollection = interf.GetMembers().ToList();
+                foreach (MemberInfo member in memberInfoCollection)
+                {
+                    if (member.MemberType.ToString() == "Property")
+                    {
+                        List<MemberInfo> setMethodsList = memberInfoCollection.Where(
+                            c => c.MemberType.ToString() == "Method" && c.Name.Contains("set_" + member.Name)
+                            ).ToList();
+
+                        CodeMemberProperty propertyCTM = new CodeMemberProperty();
+                        propertyCTM.Attributes = MemberAttributes.Public | MemberAttributes.Final;
+                        propertyCTM.Name = member.Name;
+                        CodeTypeReference typeRef = new CodeTypeReference(((PropertyInfo)(member)).PropertyType);
+                        propertyCTM.Type = typeRef;
+                        if (setMethodsList.Count > 0)
+                        {
+                            propertyCTM.SetStatements.Add(cs1);
+                        }
+                        propertyCTM.GetStatements.Add(cs1);
+                        jazClass.Members.Add(propertyCTM);
+                    }
+                    else if (member.MemberType.ToString() == "Method")
+                    {
+                        List<MemberInfo> setMethodsListForProperty = memberInfoCollection.Where(
+                         c => c.MemberType.ToString() == "Property" &&
+                             (member.Name.Contains("set_" + c.Name) || member.Name.Contains("get_" + c.Name))
+                         ).ToList();
+                        if (setMethodsListForProperty.Count() == 0)
+                        {
+                            CodeMemberMethod methodCTM = new CodeMemberMethod();
+                            methodCTM.Name = member.Name;
+                            methodCTM.Attributes = MemberAttributes.Public | MemberAttributes.Final;
+                            CodeTypeReference methodTypeRef = new CodeTypeReference(((MethodInfo)(member)).ReturnType);
+                            methodCTM.ReturnType = methodTypeRef;
+
+                            List<ParameterInfo> methodParameters = ((System.Reflection.MethodInfo)(member)).GetParameters().ToList();
+                            CodeParameterDeclarationExpressionCollection parameterCollection = new CodeParameterDeclarationExpressionCollection();
+                            foreach (ParameterInfo info in methodParameters)
+                            {
+                                CodeParameterDeclarationExpression expresion = new CodeParameterDeclarationExpression(info.ParameterType, info.Name);
+                                parameterCollection.Add(expresion);
+                            }
+                            methodCTM.Parameters.AddRange(parameterCollection); 
+                            jazClass.Members.Add(methodCTM);
+                            methodCTM.Statements.Add(cs1);
+                        }
+                    }
+                }
+            }
             samples.Types.Add(jazClass);
 
-            // Declare a new code entry point method.
-            //CodeEntryPointMethod start = new CodeEntryPointMethod();
-
-            // Create a type reference for the System.Console class.
-            //CodeTypeReferenceExpression csSystemConsoleType = new CodeTypeReferenceExpression("System.Console");
-
-            //CodeMethodInvokeExpression cs1 = new CodeMethodInvokeExpression(
-            //    csSystemConsoleType, "WriteLine",
-            //    new CodePrimitiveExpression("Hello World!"));
-            //start.Statements.Add(cs1);
-
-            // Add the code entry point method to
-            // the Members collection of the type.
-            //jazClass.Members.Add(start);
-
-            return compileUnit;
+           return compileUnit;
         }
 
         public static void GenerateCode(CodeDomProvider provider,
