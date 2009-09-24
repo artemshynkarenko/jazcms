de902588c084c6c8f2cb125e6451b80d951b7fbf
ku3necvakula <ku3necvakula@748823f4-67e9-11de-873c-fb8c264d332a>
2009-09-24 09:35:58 +0000

    Added WPF user interface.

---
+++ trunk/JazCms.WebProject.WpfEditor/SettingsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JazCms.WebProject.WpfEditor
+{
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsPage : Window
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected void buttonExit_Close(object sender,EventArgs arg)
+        {
+            this.Close();
+        }
+    }
+}
